2964e8a77b85ee62664031bbdd0af680ae16d1c9
wilfriedlesauvage <obistrip@outlook.fr>
2020-05-19 11:14:07 +0200

    maj

--- canvas_exo4/assets/js/script.js
+++ canvas_exo4/assets/js/script.js
@@ -2,20 +2,23 @@
         const ctx = canvas.getContext("2d");
 
 
-        ctx.fillStyle = "#de9400";
+        
         ctx.beginPath();
+        ctx.fillStyle = "#de9400";
         ctx.moveTo(230, 650); // on dirige le point
         ctx.lineTo(100, 400); // on trace la ligne (invisible si on ne déclare pas de couleurs)
         ctx.lineTo(350, 400); // 
-        ctx.closePath();
         ctx.fill(); //  
+        ctx.closePath();
             
         ctx.beginPath();
-        ctx.strokeStyle='brown';
-        ctx.lineWidth=4;
+        ctx.fillStyle='#976917';
+        ctx.strokeStyle='#976917';
         ctx.moveTo(100,400);
         ctx.quadraticCurveTo(220, 80, 350, 400);
         ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
         
 
             
